80b6a0f4a40fda85b4abf9ab9616401ecad8b9c7
projeto_django <deividbertapele@gmail.com>
2022-10-06 12:19:20 -0300

    atualização do dotenv

--- aula_ninja1/settings.py
+++ aula_ninja1/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yqzqe148@udwq!0iqeo9!adi5opa+y*_(3^&$=)w7-eu7n9+_4"
+SECRET_KEY = os.getenv("SECRET KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
